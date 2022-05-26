5d0fdd9277c4ec72e1b19b340129b080426c6717
Dany <danymike.joseph@gmail.com>
2022-05-26 14:41:15 +0200

    feat: header integration mobile done

--- src/components/organisms/o-header.vue
+++ src/components/organisms/o-header.vue
@@ -5,7 +5,15 @@
       <div
         class="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10"
       >
-        <div class="flex justify-start lg:w-0 lg:flex-1">
+        <a href="#">
+          <span class="sr-only">Workflow</span>
+          <img
+            class="h-8 w-auto sm:h-10 icon-desktop"
+            src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+            alt=""
+          />
+        </a>
+        <div class="flex justify-start lg:w-0 lg:flex-1 items-center auth-container-mobile">
           <a href="#">
             <span class="sr-only">Workflow</span>
             <img
@@ -13,6 +21,20 @@
               src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
               alt=""
             />
+          </a>
+          <ShoppingCartIcon :size="8" class="w-10 h-10 ml-4 lg:hidden" />
+          <HeartIcon :size="8" class="w-10 h-10 ml-2 lg:hidden" />
+          <a
+            href="#"
+            class="whitespace-nowrap text-base font-medium text-gray-500 hover:text-gray-900 lg:hidden ml-8"
+          >
+            Sign in
+          </a>
+          <a
+            href="#"
+            class="ml-8 whitespace-nowrap inline-flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 lg:hidden"
+          >
+            Sign up
           </a>
         </div>
         <div class="-mr-2 -my-2 md:hidden">
@@ -40,7 +62,7 @@
                 aria-hidden="true"
               />
             </PopoverButton>
-            
+
             <transition
               enter-active-class="transition ease-out duration-200"
               enter-from-class="opacity-0 translate-y-1"
@@ -50,7 +72,7 @@
               leave-to-class="opacity-0 translate-y-1"
             >
               <PopoverPanel
-                class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 w-screen max-w-md sm:px-0"
+                class="absolute z-10 left-1/2 transform -translate-x-1/2 mt-3 px-2 max-w-md sm:px-0"
               >
                 <div class="rounded-lg shadow-lg ring-1 ring-black ring-opacity-5 overflow-hidden">
                   <div class="px-5 py-5 bg-gray-50 sm:px-8 sm:py-8">
@@ -59,20 +81,21 @@
                         Categories
                       </h3>
                       <ul role="list" class="mt-4 space-y-4">
-                        <li v-for="post in recentPosts" :key="post.id" class="text-base truncate">
-                          <a
-                            :href="post.href"
-                            class="font-medium text-gray-900 hover:text-gray-700"
+                        <li
+                          v-for="category in categories"
+                          :key="category.id"
+                          class="text-base truncate"
+                        >
+                          <router-link
+                            :to="`/category/${category.id}`"
+                            class="font-medium text-gray-900 hover:text-gray-700 w-full"
                           >
-                            {{ post.name }}
-                          </a>
+                            <span class="w-full">
+                              {{ category.name }}
+                            </span>
+                          </router-link>
                         </li>
                       </ul>
-                    </div>
-                    <div class="mt-5 text-sm">
-                      <a href="#" class="font-medium text-indigo-600 hover:text-indigo-500">
-                        View all posts <span aria-hidden="true">&rarr;</span></a
-                      >
                     </div>
                   </div>
                 </div>
@@ -81,7 +104,7 @@
           </Popover>
         </PopoverGroup>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
-          <ShoppingCartIcon :size="8" class="w-10 h-10 mr-5"/>
+          <ShoppingCartIcon :size="8" class="w-10 h-10 mr-5" />
           <HeartIcon :size="8" class="w-10 h-10 mr-5" />
           <a
             href="#"
@@ -122,6 +145,7 @@
                   src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
                   alt="Workflow"
                 />
+                <!-- mobile navigation content here -->
               </div>
               <div class="-mr-2">
                 <PopoverButton
@@ -154,14 +178,59 @@
   </Popover>
 </template>
 
-<script setup>
+<script>
 import { Popover, PopoverButton, PopoverGroup, PopoverPanel } from '@headlessui/vue'
-import { MenuIcon, XIcon, ShoppingCartIcon, HeartIcon} from '@heroicons/vue/outline'
+import { MenuIcon, XIcon, ShoppingCartIcon, HeartIcon } from '@heroicons/vue/outline'
 import { ChevronDownIcon } from '@heroicons/vue/solid'
+import { FETCH_CATEGORIES } from '@/store/modules/categories/types'
+import { mapGetters } from 'vuex'
 
-const recentPosts = [
-  { id: 1, name: 'Boost your conversion rate', href: '#' },
-  { id: 2, name: 'How to use search engine optimization to drive traffic to your site', href: '#' },
-  { id: 3, name: 'Improve your customer experience', href: '#' },
-]
+export default {
+  components: {
+    MenuIcon,
+    XIcon,
+    ShoppingCartIcon,
+    HeartIcon,
+    ChevronDownIcon,
+    Popover,
+    PopoverButton,
+    PopoverGroup,
+    PopoverPanel,
+  },
+  data() {
+    return {
+      recentPosts: [
+        { id: 1, name: 'Boost your conversion rate', href: '#' },
+        {
+          id: 2,
+          name: 'How to use search engine optimization to drive traffic to your site',
+          href: '#',
+        },
+        { id: 3, name: 'Improve your customer experience', href: '#' },
+      ],
+    }
+  },
+  mounted() {
+    this.$store.dispatch(`${FETCH_CATEGORIES}`)
+  },
+  computed: {
+    ...mapGetters({
+      categories: 'getCategoryResponse',
+    }),
+  },
+}
 </script>
+
+<style scoped>
+@media (min-width: 768px) {
+  .auth-container-mobile {
+    display: none !important;
+  }
+}
+
+@media (max-width: 768px) {
+  .icon-desktop {
+    display: none !important;
+  }
+}
+</style>
