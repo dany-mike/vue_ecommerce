494625ec88490b1863e7c83081ac120db291090f
Dany <danymike.joseph@gmail.com>
2022-06-01 23:12:25 +0200

    feat: cart wip

--- src/pages/CartPage.vue
+++ src/pages/CartPage.vue
@@ -6,71 +6,7 @@
         <h2 id="cart-heading" class="sr-only">Items in your shopping cart</h2>
 
         <ul role="list" class="border-t border-b border-gray-200 divide-y divide-gray-200">
-          <li class="flex py-6 sm:py-10">
-            <div class="flex-shrink-0">
-              <img
-                src="https://tailwindui.com/img/ecommerce-images/shopping-cart-page-01-product-01.jpg"
-                alt="Front of men&#039;s Basic Tee in sienna."
-                class="w-24 h-24 rounded-md object-center object-cover sm:w-48 sm:h-48"
-              />
-            </div>
-
-            <div class="ml-4 flex-1 flex flex-col justify-between sm:ml-6">
-              <div class="relative pr-9 sm:grid sm:grid-cols-2 sm:gap-x-6 sm:pr-0">
-                <div>
-                  <div class="flex justify-between">
-                    <h3 class="text-sm">
-                      <a href="#" class="font-medium text-gray-700 hover:text-gray-800">
-                        Basic Tee
-                      </a>
-                    </h3>
-                  </div>
-                  <p class="mt-1 text-sm font-medium text-gray-900">32.00€</p>
-                </div>
-
-                <div class="mt-4 sm:mt-0 sm:pr-9">
-                  <label for="quantity-0" class="sr-only">Quantity, Basic Tee</label>
-                  <select
-                    id="quantity-0"
-                    name="quantity-0"
-                    class="max-w-full rounded-md border border-gray-300 py-1.5 text-base leading-5 font-medium text-gray-700 text-left shadow-sm focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
-                  >
-                    <option value="1">1</option>
-                    <option value="2">2</option>
-                    <option value="3">3</option>
-                    <option value="4">4</option>
-                    <option value="5">5</option>
-                    <option value="6">6</option>
-                    <option value="7">7</option>
-                    <option value="8">8</option>
-                  </select>
-
-                  <div class="absolute top-0 right-0">
-                    <button
-                      type="button"
-                      class="-m-2 p-2 inline-flex text-gray-400 hover:text-gray-500"
-                    >
-                      <span class="sr-only">Remove</span>
-                      <!-- Heroicon name: solid/x -->
-                      <svg
-                        class="h-5 w-5"
-                        xmlns="http://www.w3.org/2000/svg"
-                        viewBox="0 0 20 20"
-                        fill="currentColor"
-                        aria-hidden="true"
-                      >
-                        <path
-                          fill-rule="evenodd"
-                          d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                          clip-rule="evenodd"
-                        />
-                      </svg>
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </li>
+          <MCartItems :cart-items="cart" @order-total="setOrderTotal" />
         </ul>
       </section>
 
@@ -82,7 +18,7 @@
         <dl class="mt-6 space-y-4">
           <div class="border-t border-gray-200 pt-4 flex items-center justify-between">
             <dt class="text-base font-medium text-gray-900">Order total</dt>
-            <dd class="text-base font-medium text-gray-900">$112.32</dd>
+            <dd class="text-base font-medium text-gray-900">{{ orderTotal }}€</dd>
           </div>
         </dl>
 
@@ -100,7 +36,26 @@
 </template>
 
 <script>
+import MCartItems from '@/components/molecules/m-cart-items.vue'
+import { mapGetters } from 'vuex'
 export default {
+  components: { MCartItems },
   name: 'CartPage',
+  emits: ['order-total'],
+  data() {
+    return {
+      orderTotal: '',
+    }
+  },
+  computed: {
+    ...mapGetters({
+      cart: 'getCart',
+    }),
+  },
+  methods: {
+    setOrderTotal(orderTotal) {
+      this.orderTotal = orderTotal
+    },
+  },
 }
 </script>
--- src/store/modules/cart/actions.js
+++ src/store/modules/cart/actions.js
@@ -1,26 +1,10 @@
 import * as types from './types'
 
 export default {
-  //   [types.ADD_TO_CART]({ commit }, body) {
-  //   },
   [types.GET_CART]({ commit }) {
-    const products = localStorage.getItem('products')
-    commit(types.SET_CART, products)
+    if (localStorage.getItem('products')) {
+      const products = JSON.parse(localStorage.getItem('products'))
+      commit(types.SET_CART, products)
+    }
   },
-  //   [types.DELETE_CART_ITEM]({ commit }) {
-
-  //   },
-
-  //   [types.UPDATE_QUANTITY_ITEM]({ commit }) {
-
-  //   },
-  //   TODO: add delete image from my backoffice
-  //   [types.DELETE_IMAGE]({ commit }, { userId, productId }) {
-  //     console.log(commit)
-  //     return axios
-  //       .delete(`${process.env.VUE_APP_API_BASE_URL}/wishlist/${userId}/${productId}`, {
-  //         headers: authHeader(),
-  //       })
-  //       .catch((err) => console.log(err))
-  //   },
 }
