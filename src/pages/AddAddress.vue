d6a610a1e30ea71baeb2989ff02f0c46b29d060b
Dany <danymike.joseph@gmail.com>
2022-06-05 22:00:12 +0200

    feature: add handle back method

--- src/pages/AddAddress.vue
+++ src/pages/AddAddress.vue
@@ -33,13 +33,33 @@
         :classValue="'appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm'"
       />
     </div>
+    <div class="mt-8">
+      <AButton
+        :classValue="'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bgw-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'"
+      >
+        Add {{ addressPageTitle() }}
+      </AButton>
+    </div>
+    <div class="mt-4">
+      <AButton
+        @click="handleBack"
+        :classValue="'w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-red-600 hover:bgw-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'"
+      >
+        Back
+      </AButton>
+    </div>
   </div>
 </template>
 
 <script>
 import AInput from '@/components/atoms/a-input.vue'
+import AButton from '@/components/atoms/a-button.vue'
 export default {
   name: 'AddAddress',
+  components: {
+    AInput,
+    AButton,
+  },
   data() {
     return {
       routerValue: null,
@@ -61,14 +81,16 @@ export default {
       },
     }
   },
-  components: {
-    AInput,
-  },
   methods: {
     addressPageTitle() {
       return this.$router.currentRoute._value.query.type === 'shipping'
         ? 'shipping address'
         : 'billing address'
+    },
+    handleBack() {
+      this.$router.currentRoute._value.query.from === 'checkout'
+        ? this.$router.push('checkout')
+        : this.$router.push('my-account')
     },
   },
 }
