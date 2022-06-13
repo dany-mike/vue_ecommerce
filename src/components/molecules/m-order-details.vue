300f5898dd49b3ba4e6a0e18f7086ed2a61af22d
Dany <danymike.joseph@gmail.com>
2022-06-13 23:12:15 +0200

    feat: m-order details component wip

--- src/components/molecules/m-order-details.vue
+++ src/components/molecules/m-order-details.vue
@@ -1,5 +1,41 @@
 <template>
   <div class="m-order-details">
+    <div
+      class="m-orders-details w-full mt-10 block p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700"
+    >
+      <div class="flex justify-between mb-1">
+        <span class="text-base font-medium dark:text-white" :style="{ color: fItem.color }">{{
+          item.status
+        }}</span>
+      </div>
+      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+        <div
+          class="h-2.5 rounded-full"
+          :style="{ width: fItem.width, backgroundColor: fItem.color }"
+        ></div>
+      </div>
+      <p class="font-normal text-gray-700 dark:text-gray-400 mt-2">
+        Total incl tax: {{ item.totalPrice }}€
+      </p>
+      <p class="font-normal text-gray-700 dark:text-gray-400 mt-2">
+        Subtotal: {{ item.subtotal }}€
+      </p>
+      <p class="font-normal text-gray-700 dark:text-gray-400 mt-2">Tax: {{ item.tax }}€</p>
+      <AButton
+        :classValue="'bg-indigo-500 mt-2 w-full'"
+        v-if="item.status === 'COMPLETE'"
+        @click="handlePaymentRoute(item)"
+      >
+        Pay this order
+      </AButton>
+      <AButton
+        :classValue="'bg-indigo-500 mt-2 w-full'"
+        v-if="item.status === 'CREATED'"
+        @click="handleCheckoutRoute(item)"
+      >
+        Complete my order
+      </AButton>
+    </div>
     <AButton :classValue="'bg-indigo-500 mt-2 w-full'" @click="emitOrderList">
       Back to orders list
     </AButton>
@@ -13,10 +49,56 @@ export default {
   components: {
     AButton,
   },
+  mounted() {},
+  props: {
+    item: {
+      type: Object,
+      default: () => {},
+    },
+  },
+  computed: {
+    fItem() {
+      const fItem = {}
+      return this.getFItem(fItem, this.item.status)
+    },
+  },
   emits: ['m-order-details'],
   methods: {
+    getFItem(element, status) {
+      let color = 'green'
+      let width = ''
+      if (status === 'CANCEL') {
+        color = 'red'
+        width = '100%'
+      }
+
+      if (status === 'CREATED') {
+        width = '33%'
+      }
+
+      if (status === 'COMPLETE') {
+        width = '66%'
+      }
+
+      if (status === 'PAID') {
+        width = '100%'
+      }
+
+      element = {
+        ...this.item,
+        color,
+        width,
+      }
+      return element
+    },
     emitOrderList() {
       this.$emit('m-order-details')
+    },
+    handleCheckoutRoute(item) {
+      this.$router.push(`/checkout/${item.id}`)
+    },
+    handlePaymentRoute(item) {
+      this.$router.push(`/payment/${item.id}`)
     },
   },
 }
